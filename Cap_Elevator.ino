354bfd9f5f53815aa5bc7de20960ff13b77c5ee4
MasSagita <40248554+MasSagita@users.noreply.github.com>
2023-06-09 10:54:56 +0700

    Add files via upload

---
+++ Cap_Elevator.ino
@@ -0,0 +1,188 @@
+#include <EEPROM.h>
+
+#include "input_output.h"
+
+int pw_speed;
+
+int ir_stop;
+
+void (* resetFunc) (void) = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pw_speed = EEPROM.read(0);
+
+  Serial.begin(115200);
+  Serial.println("Cap Elevator Orienter");
+
+  setup_input_output();
+
+  standby();
+}
+
+int menu = 0;
+int cursor_menu = 0;
+
+int dummy_speed = 0;
+
+int delay_ir = 1000;
+
+int smooth_start_speed = 0;
+
+bool masuk_standby = false;
+
+int last_counter_cap;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led_pin, !digitalRead(led_pin));
+  refresh_screen(5);
+
+
+  last_counter_cap = total_cap;
+
+  lcd.setCursor(0, 0), lcd.print(F("    Elev Run"));
+  lcd.setCursor(0, 1), lcd.print(F("S:")), lcd.print(pw_speed);
+  lcd.setCursor(7, 1), lcd.print(total_cap);
+  lcd.setCursor(12, 1), lcd.print(F("IR:")), lcd.print(ir_state());
+
+
+  if (button(0)) pw_speed += 5, beep_buz(1, 25);
+  if (button(1)) pw_speed -= 5, beep_buz(1, 25);
+
+  if (pw_speed > 255) pw_speed = 255;
+  if (pw_speed < 0) pw_speed = 0;
+
+  dummy_speed = pw_speed;
+
+  counter_cap();
+
+  if (ir_state()) {
+    lcd.clear();
+    beep_buz(1, 25);
+    bool cap_full = true;
+    unsigned long prev_millis = millis();
+    //Serial.println(millis() - prev_millis);
+    smooth_start_speed = 0;
+    while (1) {
+      lcd.setCursor(0, 0), lcd.print(F("    Cap Full"));
+      if (!ir_state()) {
+        cap_full = false;
+        if (smooth_start_speed >= pw_speed || dummy_speed == pw_speed) break;
+        if (smooth_start_speed == 0) beep_buz(1, 500);
+        smooth_transition(&smooth_start_speed, pw_speed, 5, 25);
+        run_motor(smooth_start_speed, 1);
+      }
+      if (cap_full && millis() - prev_millis >= delay_ir) {
+        lcd.setCursor(0, 1), lcd.print(F("    ElevStop"));
+        // variable to smooth, target, step, interval(ms)
+        smooth_transition(&dummy_speed, 0, 5, 5);
+        run_motor(dummy_speed, 1);
+        //cap_full = true;
+        if (button(2)) masuk_standby = false, standby();
+      }
+      if (dummy_speed == 0 && smooth_start_speed == 0) {
+        led_blink(100, 1000);
+        digitalWrite(buz_pin, HIGH), delay(100);
+        digitalWrite(buz_pin, LOW), delay(1000);
+      }
+    }
+
+  }
+  if (!ir_state()) run_motor(pw_speed, 1);
+
+  if (button(2)) masuk_standby = false, standby();
+}
+
+void standby() {
+  lcd.clear();
+  while (!masuk_standby) {
+    led_blink(100, 100);
+    smooth_transition(&dummy_speed, 0, 5, 20);
+    run_motor(dummy_speed, 1);
+    lcd.setCursor(0, 0), lcd.print(F(" Elevator Stop"));
+    lcd.setCursor(0, 1), lcd.print(F("      S:")), lcd.print(dummy_speed);
+    if (dummy_speed == 0) {
+      beep_buz(1, 500), masuk_standby = true;
+      break;
+    }
+  }
+
+  while (masuk_standby) {
+    refresh_screen(3);
+    lcd.setCursor(0, 0), lcd.print(F("Elevator StandBy"));
+    lcd.setCursor(11, 1), lcd.print(total_cap);
+    lcd.setCursor(15, 1), lcd.print(ir_state());
+
+    if (button(0) && button(1)) save();
+
+    if (!button(0) && !button(1)) {
+      led_blink(25, 1000);
+      lcd.setCursor(0, 1), lcd.print(F("OFF :"));
+      lcd.print(F("0")), run_motor(0, 1);
+    }
+
+    if (button(0)) {
+      led_blink(50, 100);
+      lcd.setCursor(0, 1), lcd.print(F("UP  :"));
+      run_motor(pw_speed, 1), lcd.print(pw_speed);
+    }
+    if (button(1)) {
+      led_blink(100, 50);
+      lcd.setCursor(0, 1), lcd.print(F("DOWN:"));
+      run_motor(-pw_speed, 1), lcd.print(-pw_speed);
+    }
+
+    if (button(2)) {
+      lcd.clear();
+      while (1) {
+        lcd.setCursor(0, 0), lcd.print(F(" Elevator Start"));
+        lcd.setCursor(0, 1), lcd.print(F("      S:")), lcd.print(smooth_start_speed);
+        led_blink(100, 100);
+        if (smooth_start_speed == 0) beep_buz(2, 50);
+        smooth_transition(&smooth_start_speed, pw_speed, 5, 20);
+        run_motor(smooth_start_speed, 1);
+        if (smooth_start_speed >= pw_speed) break;
+      }
+      if (smooth_start_speed >= pw_speed) {
+        delay(100);
+        smooth_start_speed = 0;
+        break;
+      }
+    }
+  }
+}
+
+void smooth_transition(int* value, int target, int stp, unsigned long interval) {
+  static unsigned long previousTime = 0; // Previous time
+
+  unsigned long currentTime = millis(); // Current time
+  unsigned long elapsedTime = currentTime - previousTime; // Elapsed time
+
+  if (elapsedTime >= interval) {
+    previousTime = currentTime; // Update the previous time
+    if (*value < target) {
+      *value += stp;
+      if (*value > target) *value = target;
+    }
+    else if (*value > target) {
+      *value -= stp;
+      if (*value < target) *value = target;
+    }
+    //Serial.print("Current value: ");
+    //Serial.println(*value);
+  }
+}
+
+void save() {
+  beep_buz(2, 400);
+  delay(500), lcd.clear();
+  lcd.setCursor(0, 0), lcd.print("  Save & Rstrt");
+  EEPROM.write(0, pw_speed);
+  for (int i = 0; i < 16; i++) {
+    lcd.setCursor(i, 1), lcd.print(F("."));
+    delay(100);
+  }
+  resetFunc();
+}
+
---
+++ input_output.h
@@ -0,0 +1,174 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+//char panah
+byte customChar[] = {
+  B00000,
+  B00100,
+  B00010,
+  B11111,
+  B00010,
+  B00100,
+  B00000,
+  B00000
+};
+
+int btn_pin[3] = {2, 3, 4};
+
+int ir_pin = A0;
+
+const int led_pin = 13;
+const int buz_pin = 12;
+
+const int motor_pwm_pin = 9;
+const int motor_left_pin = 10;
+const int motor_right_pin = 11;
+
+void setup_input_output() {
+  lcd.init();
+
+  for (int i = 0; i < 3; i++) {
+    pinMode(btn_pin[i], INPUT);
+  }
+  pinMode(ir_pin, INPUT);
+  pinMode(led_pin, OUTPUT);
+  pinMode(buz_pin, OUTPUT);
+  
+  pinMode(motor_pwm_pin, OUTPUT);
+  pinMode(motor_left_pin, OUTPUT);
+  pinMode(motor_right_pin, OUTPUT);
+  
+  for (int i = 0; i < 6; i++) {
+    digitalWrite(buz_pin, !digitalRead(buz_pin));
+    digitalWrite(led_pin, !digitalRead(led_pin));
+    delay(30);
+  }
+  
+  lcd.createChar(0, customChar);
+  lcd.backlight();
+  lcd.setCursor(0, 0), lcd.print(F(" CAP's ELEVATOR"));
+  delay(500);
+  lcd.clear();
+}
+
+void run_motor(int spd, int brk) {
+  if (spd == 0 && brk == 1) {
+    analogWrite(motor_pwm_pin, 255);
+    digitalWrite(motor_left_pin, 0);
+    digitalWrite(motor_right_pin, 0);
+  }
+  if (spd == 0 && brk == 0) {
+    analogWrite(motor_pwm_pin, 0);
+    digitalWrite(motor_left_pin, 0);
+    digitalWrite(motor_right_pin, 0);
+  }
+  if (spd > 0) {
+    analogWrite(motor_pwm_pin, spd);
+    digitalWrite(motor_left_pin, 1);
+    digitalWrite(motor_right_pin, 0);
+  }
+  if (spd < 0) {
+    analogWrite(motor_pwm_pin, -spd);
+    digitalWrite(motor_left_pin, 0);
+    digitalWrite(motor_right_pin, 1);
+  }
+}
+
+bool button(int ch) {
+  return !digitalRead(btn_pin[ch]); 
+}
+
+bool ir_state() {
+  return !digitalRead(ir_pin); 
+}
+
+int last_state_ir = HIGH;
+int ir_counter = 0;
+int total_cap = 0;
+
+void counter_cap() {
+  if (!last_state_ir && ir_state()) {
+    ir_counter += 1;
+    total_cap += 1;
+  }
+  last_state_ir = ir_state();
+}
+
+unsigned long prev_millis_led;
+int led_state = LOW;
+void led_blink(int on_time, int off_time) {
+  unsigned long current_millis_led = millis();
+  if ((led_state == 1) && (current_millis_led - prev_millis_led >= on_time)) {
+    led_state = 0;
+    prev_millis_led = current_millis_led;
+  }
+  else if ((led_state == 0) && (current_millis_led - prev_millis_led >= off_time)) {
+    led_state = 1;
+    prev_millis_led = current_millis_led;
+  }
+  digitalWrite(led_pin, led_state);
+  //if(ch == 1) digitalWrite(buzzer_pin, led_state);
+}
+
+void beep_buz(int sum, int interval) {
+  if (sum > 0) sum = sum * 2;
+  if (interval >= 500) interval = 500;
+  for (int i = 0; i < sum; i++) {
+    digitalWrite(buz_pin, !digitalRead(buz_pin));
+    delay(interval);
+  }
+}
+
+
+
+const int short_press_time = 500;
+const int long_press_time = 1000;
+
+int last_state_rtry = LOW;  // previous state
+int current_state_rtry;     // current reading
+unsigned long pressed_time = 0;
+unsigned long realesed_time = 0;
+bool is_pressing = false;
+bool is_long_pressing = false;
+
+bool long_press_btn = false;
+bool short_press_btn = false;
+
+void short_long_ir(int pin) {
+  current_state_rtry = pin;
+  if (last_state_rtry == HIGH && current_state_rtry == LOW) {
+    pressed_time = millis();
+    is_pressing = true;
+    is_long_pressing = false;
+  } else if (last_state_rtry == LOW && current_state_rtry == HIGH) {
+    is_pressing = false;
+    realesed_time = millis();
+    long press_duration = realesed_time - pressed_time;
+    if (press_duration < short_press_time) {
+      long_press_btn = false;
+      short_press_btn = true;
+      Serial.print("short press ");
+      Serial.println(pressed_time);
+    }
+  }
+  if (is_pressing == true && is_long_pressing == false) {
+    long press_duration = millis() - pressed_time;
+    if (press_duration > long_press_time) {
+      long_press_btn = true;
+      short_press_btn = false;
+      Serial.print("long press");
+      Serial.println(pressed_time);
+      is_long_pressing = true;
+    }
+  }
+  last_state_rtry = current_state_rtry;
+}
+
+int refresh;
+void refresh_screen(int intervalRefresh) {
+  if (++refresh > intervalRefresh) {
+    lcd.clear();
+    refresh = 0;
+  }
+}
+
